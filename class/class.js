21b33970189b51a7254504ee83aa7fb4d3fcdefd
Arnab682 <arnab.das682@gmail.com>
2023-02-25 22:54:50 +0600

    vue js practice

---
+++ class/class.js
@@ -0,0 +1,11 @@
+let cls = new Vue({
+    el: "#class",
+    data:{
+        name: 'Class',
+    },
+    methods:{
+        a(){
+            return 'practice';
+        }
+    }
+})
---
+++ print/app.js
@@ -0,0 +1,6 @@
+let app1 = new Vue({
+    el: '.age',
+    data: {
+        age: '28'
+    }
+})
